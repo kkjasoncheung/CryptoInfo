857448e8aff3dcd43de3e3e3b264475b73f51a85
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-01-14 02:25:28 -0500

    finished implementation of the trendline chart for the litecoin page

--- CryptoInfo/src/app/litecoin-data.service.ts
+++ CryptoInfo/src/app/litecoin-data.service.ts
@@ -7,13 +7,41 @@ export class LitecoinDataService {
   constructor(private http:HttpClient) { }
 
   tradingPairs = {};
+  priceInLastHour = [];
+  secondsInLastHour = [];
+
+  // clear data for new API request
+  clearData() {
+    this.tradingPairs = {};
+    this.priceInLastHour = [];
+    this.secondsInLastHour = [];
+  }
 
   retrieveData() {
-  	// get pricing info for LTC from CryptoCompare
+  	// get current pricing info for LTC from CryptoCompare
   	this.http.get('https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=ETH,USD,BTC,BCH').subscribe(
   		(data) => {
   			this.tradingPairs = data;
   		}
   	);
   }
+
+  // get price for every 1 minute in the last hour
+  retrieveMinuteData(){
+    var currency = 'USD';
+    // query CryptoCompare API to get data
+    this.http.get('https://min-api.cryptocompare.com/data/histominute?fsym=LTC&tsym='+ currency + '&limit=60&aggregate=3&e=CCCAGG').subscribe(
+      (minuteData) => {
+        // add each price to the priceInLastHour array
+        for (var i=0; i<minuteData.Data.length; i++) {
+          this.priceInLastHour.push(parseFloat(minuteData.Data[i].close));
+          // convert into dates before pushing to array
+          var tempDate = new Date(0);
+          tempDate.setUTCSeconds(parseInt(minuteData.Data[i].time));
+          this.secondsInLastHour.push(tempDate.toLocaleTimeString());
+        }
+      }
+    );
+  }
+
 }
--- CryptoInfo/src/app/litecoin/litecoin.component.ts
+++ CryptoInfo/src/app/litecoin/litecoin.component.ts
@@ -11,7 +11,54 @@ export class LitecoinComponent implements OnInit {
   constructor(private ltcData:LitecoinDataService) { }
 
   ngOnInit() {
+  	this.ltcData.clearData();
   	this.ltcData.retrieveData();
+  	this.ltcData.retrieveMinuteData();
+  	this.drawChart();
+  }
+
+
+  // draw trendline chart
+  drawChart(){
+	  // universal JS implement trend chart for all components
+	  var trendContext = document.getElementById("trend-chart").getContext('2d');
+	  var myTrendline = new Chart(trendContext, {
+	    type: 'line',
+	    data: {
+	    	labels: this.ltcData.secondsInLastHour,
+	        datasets: [{
+	            label: 'USD Price',
+	            data: this.ltcData.priceInLastHour,
+	            backgroundColor: [
+	                'rgb(236, 236, 236)'
+	            ],
+	            borderColor: [
+	                'rgb(181, 181, 181)'
+	            ],
+	            borderWidth: 1,
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	               	  suggestedMin: 250,
+	                }
+	            }]
+	        },
+	        elements: {
+	        	point: {
+	        		radius: 0
+	        	}
+	        },
+	        legend: {
+	        	onClick: function(){}
+	        },
+	    },
+	  });
+
+	  // fix graph not showing upon loading
+	  setTimeout(function() { myTrendline.update(); },10);
   }
 
 }
